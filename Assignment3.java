b2d25cf9adc904e09570a7796ff413df8255faa0
Kevin Candelaria <kevdashdev@gmail.com>
2022-02-19 23:32:43 +0800

    Handle out of bounds

--- Assignment3.java
+++ Assignment3.java
@@ -8,7 +8,7 @@ class Assignment3 {
         System.out.print("How many columns? ");
         int columns = sc.nextInt();
         sc.nextLine();
-        System.out.print("Enter origin coordinate? ");
+        System.out.print("Enter origin coordinate: ");
         String[] target;
         target = sc.nextLine().split(" ");
         String x = target[0];
@@ -16,12 +16,16 @@ class Assignment3 {
         String y = target[1];
         int yCoordinate = Integer.parseInt(y);
         sc.close();
-        System.out.println("Rows: " + rows);
-        System.out.println("Columns: " + columns);
-        System.out.println("X: " + x);
-        System.out.println("Y: " + y);
-        System.out.println("Table:");
 
+        try {
+            if (xCoordinate > columns || xCoordinate < 0) { throw new Exception("X coordinate is out of column bounds!"); }
+            if (yCoordinate > rows || yCoordinate < 0) { throw new Exception("Y coordinate is out of row bounds!"); }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+
+        System.out.println("\nResult");
         for (int row = rows; row > 0; row--) {
             for (int column = 1; column <= columns; column++) {
               if (yCoordinate == row) {
@@ -29,17 +33,15 @@ class Assignment3 {
                         System.out.print("1");
                     } else {
                         int base = xCoordinate - column + 1;
-                        if (xCoordinate < column) { base = column - xCoordinate + 1; }
+                        if (xCoordinate <= column) { base = column - xCoordinate + 1; }
                         System.out.print(base);
                     }
                 } else {
-                    if (xCoordinate == column) {
-                        int base = yCoordinate - row + 1;
-                        if (yCoordinate < row) { base = row - yCoordinate + 1; }
-                        System.out.print(base);
-                    } else {
-                        System.out.print("X");
+                    int base = yCoordinate - row + 1;
+                    if (yCoordinate <= row) {
+                        base = row - yCoordinate + 1;
                     }
+                    System.out.print(base);
                 }
                 System.out.print("\t");
             }
